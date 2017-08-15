0dbc1271c1ed45aeb1c2869effbc1d75f57eb142
imaimon1 <imaimon1@binghamton.edu>
2017-08-15 15:16:33 -0400

    the code for some tree stuff
    I am fairly sure this is np-complete but it is a good exercise. not sure if this is actually how people make graphs but it works and there needs to be code that turns the list of jobs into a directed tree but that doesn't seem so interesting or fun.

--- treefun/treefun.py
+++ treefun/treefun.py
@@ -1,12 +1,14 @@
 #finding the lowest weight path through a weighted directed tree with a start point
-#note: levels are numbers cannot be negetive
+# note: levels are numbers cannot be negetive. they are used to make sure it is a tree and add some structure. 
+# they(levels) also correspond to time for requests 
 
 class DirectedTree(object):
 	def __init__(self):
+	#names are keys and the corresponding object is the values
 		self.vertex_dict = {}
 		self.edge_dict = {}
 		self.level_dict = {}
-		
+	#writing self.blank_dict[] is hard so get functions are made now i just right self.get_Blank(). 
 	def get_Vertex(self, name):
 		return self.vertex_dict[name]
 		
@@ -16,52 +18,62 @@ class DirectedTree(object):
 	def get_Level(self, name):
 		return self.level_dict[name]
 	
+	#name of edge is always (start,end) because to edges with the same endpoints and different weights are almost always redundant
 	class Edge(object):
 		def __init__(self, start, end, weight):
 			self.start = start 
 			self.end = end
 			self.weight = weight
 			
+	#lots of useful information is stored in vertex and level classes.
 	class Vertex(object):
 		def __init__(self, name, level, weight):
 			self.name = name
-			#all children of a vertex in lower levels all parents in higher levels
+			#reminder:all children of a vertex in higher levels all parents in lower levels
 			self.level = level
 			self.weight = weight
 			self.children = []
 			self.parents = []
-			#smallest level of child of a vertex
+			#smallest level of child of a vertex initialized to -1 since it cannot be negetive
+			#there is probably a more pythony way to do this but this way works and I can still have level 0 
 			self.minlevel = -1
 
 	class Level(object):
 		def __init__(self, name):
 			self.name = name
 			self.vertices = []
+			
+	#add and delete functions delete using functions written later in the code, because it makes it easier to read
 	
 	def add_Level(self, name):
 		self.level_dict[name] = self.Level(name)
-	
+		
+	#deletes all vertices in level because why else would you delete a level
 	def del_Level(self, name):
-		for vertex in self.get_Level(name).vertices:
+		for vertex in self.get_Level(name).vertices[:]:
 			self.del_Vertex(vertex)
 		del self.level_dict[name]
-		
-	def add_Vertex(self, name, level, weight=0):
+	
+	#creates level if level not already created 
+	def add_Vertex(self, name, level, weight=0): 
 		if level in self.level_dict.keys():
 			self.get_Level(level).vertices.append(name)
 		else:
 			self.add_Level(level)
 			self.get_Level(level).vertices.append(name)
 		self.vertex_dict[name]= self.Vertex(name, level, weight)
-	
+		
+	#deletes all edges using vertex and its part in level
 	def del_Vertex(self, name):
-		for vertex in self.get_Vertex(name).children:
+		for vertex in self.get_Vertex(name).children[:]:
 			self.del_Edge(name,vertex)
-		for vertex in self.get_Vertex(name).parents:
+		for vertex in self.get_Vertex(name).parents[:]:
 			self.del_Edge(vertex,name)
 		self.get_Level(self.get_Vertex(name).level).vertices.remove(name)
 		del self.vertex_dict[name]
-		
+	
+	# checks for vertex does not create new ones. 
+	#I might try a version that makes new vertices when called so someone can make a graph with just edges and autocreated levels
 	def add_Edge(self, start, end, weight=0):
 		try: int(weight)
 		except ValueError:
@@ -73,6 +85,7 @@ class DirectedTree(object):
 			return 'this edge goes from a higher level vertex to a lower level vertex'
 		# to keep only edges with smallest or largest weights because the other edge is always wasted
 		elif (start, end) in self.edge_dict.keys():
+			#other option:
 			#if get_Edge((start, end)).weight > weight:
 			 if self.get_Edge((start, end)).weight < weight:
 				self.get_Edge((start, end)).weight = weight
@@ -80,34 +93,41 @@ class DirectedTree(object):
 			self.edge_dict[(start,end)] = self.Edge(start,end,weight)
 			self.get_Vertex(start).children.append(end)
 			self.get_Vertex(end).parents.append(start)
+			#minlevel stuff here again -1 is arbitrary 
 			if self.get_Vertex(end).level < self.get_Vertex(start).minlevel or self.get_Vertex(start).minlevel==-1:
 				self.get_Vertex(start).minlevel = self.get_Vertex(end).level
 				
+	#had a key error here fixed it with copying lists before deleting elements of them			
 	def del_Edge(self, start, end):
 		del self.edge_dict[(start,end)]
-		try:
-			self.get_Vertex(start).children.remove(end)
-			self.get_Vertex(end).parents.remove(start)
-		except KeyError:
-			pass
-	
+		#try:
+		self.get_Vertex(start).children.remove(end)
+		self.get_Vertex(end).parents.remove(start)
+		#except KeyError:
+		#	pass
+		
+	#some fun fuctions that did not turn out to be useful but were hard to make
+	#note this can make the graph stop being a tree be wary 
+	#that is why there is the commented part 
 	def move_Vertex(self, name, level):
+		#if level >= self.get_Vertex(name).minlevel and minlevel != -1:
+		#	raise Exception('this will send a higher level vertex to a lower level vertex')
 		if level not in self.level_dict.keys():
 			self.add_Level(level)
 		start_Level = self.get_Vertex(name).level
 		self.get_Level(start_Level).vertices.remove(name)
 		self.get_Vertex(name).level = level
 		self.get_Level(level).vertices.append(name)
-		
+	
+	#moves all vertices from start level to new level
 	def move_Level(self,start, new):
-		#moves all vertices from start level to new level
 		list=self.get_Level(start).vertices[:]
 		for vertex in list:
-			self.move_Vertex(vertex, new)		
-	
+			self.move_Vertex(vertex, new)
+			
+	#move all vertices from start level and higher up delta levels down
+	#assumes levels are numbers 		
 	def move_Levels(self,start, delta):
-		#move all vertices from start level and higher up delta levels down
-		#assumes levels are numbers
 		sorted_keys = self.level_dict.keys()
 		sorted_keys.sort()
 		sorted_keys.reverse()
@@ -118,31 +138,40 @@ class DirectedTree(object):
 					self.move_Level(level, next)
 			except ValueError:
 				return 'cannot move levels if they are not numbers'
+			
+#functions for solving finding a maximum weight path
 
+#turns a list of tuples into a list of a number element in the tuple
 def list_of_tuples(list,number):
-	#turns a list of tuples into a list of a number element in the tuple
 	out= []
 	for element in list:
 		sub_tuple=element[number]
 		out.append(sub_tuple)
 	return out
 
+#takes a list of tuples returns tuple with highest first element
 def max_first(list):
-	#takes a list of 2-tuples returns tuple with highest first element
 	weights = list_of_tuples(list,0)
 	max_weight = max(weights)
 	max_weight_index = weights.index(max_weight)
 	tuple = list[max_weight_index]
 	return tuple
-			
+
+#takes a directed tree with weighted edges and returns a path with maximum weight by working backwards 
+#can probably be cleaned up 
+#idea is we make a dictionary entry for each vertex which contains the maximum weight path along each child.
+#then choose the maximum and send that info to the parent 
 def optimal_weighted_path_edges(tree):
-#takes a directed tree with weighted edges and returns a path with maximum weight by working backwards
+	#initialization sorting to go from the bottom level and making the dict
 	dict={}
 	for vertex in tree.vertex_dict.keys():
 		dict[vertex]=[(0,['>'])]
 	tree.level_dict.keys().sort
-	sorted_levels=tree.level_dict.keys()
+	sorted_levels=tree.level_dict.keys()[:]
 	
+	#recursive subfunction goes through bottom level and 
+	#adds information to each parent of each bottom level vertex
+	#finishes when out of levels
 	def sub_function(sorted_levels,dict):
 		bottom_level=tree.get_Level(sorted_levels.pop()).vertices
 		list=[]	
@@ -158,6 +187,7 @@ def optimal_weighted_path_edges(tree):
 		
 			if len(sorted_levels)==0:
 				list.append((max_weight_info[0], prev))
+				
 		if len(sorted_levels)==0:
 			max_weight = max_first(list)
 			max_weight[1].reverse()
@@ -169,8 +199,12 @@ def optimal_weighted_path_edges(tree):
 		return sub_function(sorted_levels,dict)	
 	return sub_function(sorted_levels,dict)
 	
+#takes a directed tree with weighted vertices and returns a path with maximum weight 
+#if it also has weighted edges replace = with += in  the for loop	
+#baisically make all edges the weight of the vertex they go to except for edges with 
+#level 0 node for those add both vertices' weights to the edge
+#level 0 is assumed lowest level 
 def optimal_weighted_path_vertices(tree):
-	#takes a directed tree with weighted vertices and returns a path with maximum weight if it also has weighted edges replace = with += in  the for loop 
 	for edge in tree.edge_dict.keys(): 
 		tree.get_Edge(edge).weight=tree.get_Vertex(tree.get_Edge(edge).end).weight
 	for top in tree.get_Level(0).vertices:
@@ -178,17 +212,24 @@ def optimal_weighted_path_vertices(tree):
 			tree.get_Edge((top,child)).weight+=tree.get_Vertex(top).weight
 	return optimal_weighted_path_edges(tree)
 
+#assumes levels are numbers
+#takes a directed tree with weighted vertices whose children are all vertices of a certain level and below and returns a path with maximum weight
+#this was created as a solution for creating a maximum weight set of nonoverlapping requests from a set of requests with weights
+#baisically organize jobs with weights on a machine. 
+#solution turns jobs which are initially vertices with weights into edges and their levels into vertices
+#from this it creates a new level system
+#simpler solution can be made it is not organized into a tree but avtually into a set of jobs and times
 def optimal_weighted_path_vertices_level(tree):
-	#assumes levels are numbers
-	#takes a directed tree with weighted vertices whose children are all vertices of a certain level and below and returns a path with maximum weight
-	#this was created as a solution for creating a maximum weight set of nonoverlapping requests from a set of requests with weights
 	newtree = DirectedTree()
 	sorted_levels = sorted(tree.level_dict.keys())
+	#new vertex at the bottom level for leaves to turn into edges that connect to this vertex
 	bottom_level = sorted_levels[-1] + 1
 	for level in tree.level_dict.keys():
 		newtree.add_Vertex(level,level,1)
 	newtree.add_Vertex(bottom_level,bottom_level,1)
 	for vertex in tree.vertex_dict.keys():
+		#turns vertex into a bunch of edges each edge between the start-level and 
+		#each level that can be reached after the job is done it has the weight of the job 
 		if tree.get_Vertex(vertex).children:
 			for level in sorted_levels:
 				if level >= tree.get_Vertex(vertex).minlevel: 
@@ -197,6 +238,7 @@ def optimal_weighted_path_vertices_level(tree):
 			newtree.add_Edge(tree.get_Vertex(vertex).level, bottom_level, tree.get_Vertex(vertex).weight)
 	soln = optimal_weighted_path_edges(newtree)
 	
+	#rewriting solution to get an output in terms of vertices not levels
 	soln_list=soln[2]
 	soln_list.pop()
 	out = []
@@ -223,7 +265,8 @@ def optimal_weighted_path_vertices_level(tree):
 		history += str(part) + ' > '
 	
 	return soln[0], history[:-3], out 
-	
+
+#quick test cases
 tree = DirectedTree()
 for i in range(0,10):
 	tree.add_Vertex('vert_'+str(i),i,1)
